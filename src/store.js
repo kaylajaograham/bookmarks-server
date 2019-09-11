5cc6e7ecb3a3ee6487d07bb806d1c6a0bcc039be
Amber Meritt <amber.merittcampbell@gmail.com>
2019-09-10 21:33:24 -0700

    completed checkpoint 10 assignment

---
+++ src/store.js
@@ -0,0 +1,16 @@
+const bookmarks = [{
+    id: 1,
+    title: '',
+    url: '',
+    description: '',
+    rating: 1
+},
+{
+    id: 2,
+    title: '',
+    url: '',
+    description: '',
+    rating: 1
+}];
+
+module.exports = bookmarks;
