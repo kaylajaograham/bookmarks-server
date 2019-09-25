c3834baa5467f73d59b6c9a53609bf746ddb73e6
kaylajaograham <kaylajaograham@gmail.com>
2019-09-25 16:50:14 -0700

    refactor including post and delete endpoints

--- src/app.js
+++ src/app.js
@@ -1,72 +1,28 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-const {NODE_ENV} = require('./config');
-const logger = require('./logger');
-const bookmarkRouter = require('./bookmark-router');
-const BookmarksService = require('./bookmarks-service');
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const validateBearerToken = require('./validate-bearer-token')
+const errorHandler = require('./error-handler')
+const bookmarksRouter = require('./bookmark-router')
 
-const app = express();
+const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-    ? 'tiny'
-    : 'common';
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
+app.use(cors())
+app.use(helmet())
+app.use(validateBearerToken)
 
-app.use(morgan(morganOption));
-app.use(helmet());
-app.use(cors());
+app.use(bookmarksRouter)
 
-app.use(function validateBearerToken(req, res, next){
-    const apiToken = process.env.API_TOKEN;
-    const authToken = req.get('Authorization');
-
-    if(!authToken || authToken.split(' ')[1] !== apiToken){
-        logger.error(`Unauthorized request to path: ${req.path}`);
-        return res.status(401).json({error: 'Unauthorized request'})
-    }
-    next()
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
 })
 
-app.get('/bookmarks', (req, res, next) => {
-    const knexInstance = req.app.get('db')
-        BookmarksService.getAllBookmarks(knexInstance)
-            .then(bookmarks => {
-             res.json(bookmarks)
-    })
-    .catch(next)
-})
+app.use(errorHandler)
 
-app.get('/bookmarks/:bookmark_id', (req, res, next) => {
-    const knexInstance = req.app.get('db')
-    BookmarksService.getById(knexInstance, req.params.bookmark_id)
-        .then(bookmark => {
-        if(!bookmark) {
-            return res.status(404).json({
-                error: { message: `Bookmark doesn't exist`}
-            })
-        }
-        res.json(bookmark)
-      })
-        .catch(next)
-})
-
-app.get('/', (req, res) =>{
-    res.send('Hello, world!');
-})
-
-app.use(bookmarkRouter);
-
-app.use(function errorHandler(error, req, res, next){
-    let response
-    if(NODE_ENV === 'production'){
-        response = {error: {message: 'server error'}}
-    } else {
-        console.log(error);
-        response = {message: error.message, error}
-    }
-    res.status(500).json(response)
-})
-
-module.exports = app;
+module.exports = app
